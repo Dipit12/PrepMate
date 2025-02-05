37a97fe8cd2dffdd38df27f608770a2358f654de
Tithi Shah <shahtithi21@gmail.com>
2025-02-05 15:05:23 +0530

    created both login and register page

--- client/frontend/src/App.jsx
+++ client/frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import react from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Login from './components/Login';
+import Register from './components/Register'
 function App(){
   return(
     <>
       <BrowserRouter>
         <Routes>
           <Route path = "/login" element = {<Login/>}></Route>
+          <Route path = "/register" element = {<Register/>}></Route>
         </Routes>
       </BrowserRouter>
     </>
