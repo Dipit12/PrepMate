c7c8ef6b5977d05a3cd1f75a2be2df6af58b0023
Tithi Shah <shahtithi21@gmail.com>
2025-02-06 03:37:33 +0530

    about us and footer pages completed

--- my-client-app/src/App.jsx
+++ my-client-app/src/App.jsx
@@ -3,6 +3,8 @@ import React from 'react'
 import Login from './components/Login' 
 import Registeration from './components/Registration'
 import { Routes, Route, Link } from 'react-router-dom'
+import AboutUs from './components/AboutUs'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
@@ -10,6 +12,8 @@ const App = () => {
       <Routes>
         <Route path = "/register" element = {< Registeration/>} />
         <Route path = "/login" element = {< Login/>} />
+        <Route path = "/about" element = {<AboutUs/>} />
+        <Route path = "/footer" element = {<Footer/>} />
       </Routes>
     </div>
   )
